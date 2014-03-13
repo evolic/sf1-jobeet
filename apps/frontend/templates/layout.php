2075593f4def78133126da80f1dda1a334126ce4
Tomasz Kuter <TKuter@grupainteger.pl>
2014-03-13 10:13:29 +0100

    - Moved JavaScript from head tag to the end of the body tag

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -6,9 +6,9 @@
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
   </head>
   <body>
     <?php echo $sf_content ?>
+    <?php include_javascripts() ?>
   </body>
 </html>
