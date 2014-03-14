d48dd34a5698214efca8334787704e23323f2653
Tomasz Kuter <TKuter@grupainteger.pl>
2014-03-14 16:38:04 +0100

    - Added additional unit tests

--- test/unit/model/JobeetCategoryTest.php
+++ test/unit/model/JobeetCategoryTest.php
@@ -2,12 +2,32 @@
 
 include(dirname(__FILE__).'/../../bootstrap/Doctrine.php');
 
-$t = new lime_test(1);
+$t = new lime_test(4);
+
+$table = Doctrine_Core::getTable('JobeetCategory');
+/* @var $table JobeetCategoryTable */
 
 $t->comment('->getSlug()');
-$category = Doctrine_Core::getTable('JobeetCategory')->createQuery()->fetchOne();
+$category = $table->createQuery()->fetchOne();
 $t->is(
   $category->getSlug(),
   Jobeet::slugify($category->getName()),
   '->getSlug() return the slug for the category'
+);
+$t->is(
+  $category->getSlug(),
+  Jobeet::slugify($category->getName()),
+  '->getSlug() return the slug for the category'
+);
+
+$t->comment('Design category');
+$t->is(
+  count($category->getActiveJobs(1)),
+  1,
+  '$category->getActiveJobs(1) return two active categories'
+);
+$t->is(
+  $category->countActiveJobs(),
+  1,
+  '$category->countActiveJobs() return 1 job in Design category'
 );
