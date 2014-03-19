71e849b886214179d00fc68a7fe50a14748f2028
Tomasz Kuter <TKuter@grupainteger.pl>
2014-03-19 13:57:23 +0100

    - Added sfDoctrineGuardPlugin plugin by executing the command:   `php symfony plugin:install sfDoctrineGuardPlugin` according to   `Day 13: The User` available at   http://symfony.com/legacy/doc/jobeet/1_4/en/13?orm=Doctrine

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -10,5 +10,6 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins('sfDoctrinePlugin');
 
     $this->enablePlugins('sfFirePHPPlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
   }
 }
